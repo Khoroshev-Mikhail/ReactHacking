cb507210d4fb752a3716520bc957992ed8864634
Mikha38 <oyks1379@yandex.ru>
2022-07-04 13:32:33 +0300

    work

--- react-hacking/src/Components/Component2.js
+++ react-hacking/src/Components/Component2.js
@@ -1,7 +1,6 @@
 import Component2__2 from "./Component2__2"
 import { Counter_Component2 } from "../Store/Store";
 import {useDispatch, useSelector} from "react-redux";
-import { useMemo } from "react";
 
 export default function Component2(props){
     const count = useSelector(state => state.counts.Component2)
